2c20295aade2160d7143d98c08398cf10f22cb7a
League Student <league@iMac-37.attlocal.net>
2019-10-14 19:56:46 -0700

    Fizzbuzz

--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -8,14 +8,18 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
-		// 3. Play the sound that many times
-
+ String input = JOptionPane.showInputDialog("How many shiny objects do you want.");
+		int num = Integer.parseInt(input);
+ // 3. Play the sound that many times
+for (int i = 0; i < num; i++) {
+	playMisterZee();
+}
 		// 1. Call the method below
 
 	}
--- src/_12_fizz_buzz/FizzBuzz.java
+++ src/_12_fizz_buzz/FizzBuzz.java
@@ -26,6 +26,25 @@ package _12_fizz_buzz;
 **/
 
 public class FizzBuzz {
-
+public static void main(String[] args) {
+	for (int i = 1; i < 21; i++) {
+		
+		if (i%3==0&&i%5==0) {
+			System.out.println("fizzbuzz");
+		}
+		else if (i%3==0) {
+		System.out.println("fizz");	
+		}
+		else if (i%5==0) {
+			System.out.println("buzz");
+		}
+		else if (i%3==0&&i%5==0) {
+			System.out.println("fizzbuzz");
+		}
+		else {
+			System.out.println(i);
+		}
+	}
+}
 }
 
